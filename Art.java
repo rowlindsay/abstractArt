8889967f408da3634d8721b0b38628ec8c4dccc4
Rowan Lindsay <rowlindsay@gmail.com>
2015-10-09 21:42:11 -0700

    Created a static Color set method to create rainbow sequential colors. Cleaned up methods.

--- Art.java
+++ Art.java
@@ -28,11 +28,29 @@ public class Art
         pen.home();
     }
     
+    public void makeSunburst(boolean rainbow)
+    {
+        Color drawColor = Color.red;
+        for (int degree=0; degree<360; degree++) //Run until 360 degrees are covered
+        {
+            if(rainbow)
+                drawColor = setRainColor(degree,drawColor);
+            pen.setColor(drawColor);
+            pen.forward(250);
+            pen.turn(180);
+            pen.up();
+            pen.forward(250);
+            pen.turnRight(179);
+            pen.down();         //Make a line then rotate to the next degree (counter clockwise)
+        }
+    }
+    
     public void drawFractals()
     {
        
     }
     
+   
     public Color userColor(String color)
     {
         if(color.equals("black"))
@@ -57,5 +75,40 @@ public class Art
             return Color.orange;
         else
             return Color.black;
+    }
+    
+    static Color setRainColor(int i,Color curCol)
+    {
+        int r = curCol.getRed();
+        int g = curCol.getGreen();
+        int b = curCol.getBlue();
+        if(i<60)
+        {
+            g += (int)(165/60);
+        }
+        else if(i<120)
+        {
+            g += (int)(90/60);
+        }
+        else if(i<180)
+        {
+            r -= (int)(205/60);
+            g -= (int)(50/60);
+            b += (int)(50/60);
+        }
+        else if(i<240)
+        {
+           r -= (int)(50/60);
+           g -= (int)(205/60);
+           b += (int)(205/60);
+        }
+        else if(i<300)
+        {
+           r += (int)(160/60);
+           g += (int)(32/60);
+           b -= (int)(15/60);
+        }
+        Color newColor = new Color(r,g,b);
+        return newColor;
     }
 }
--- Triral.java
+++ Triral.java
@@ -10,19 +10,14 @@ public class Triral
     
     public void draw(DrawingTool pen)
     {
-        int r = 255;
-        int g = 0;
-        int b = 0;
-        for(int i=360;i>0;i--)
+        Color drawColor = Color.red;
+        for(int i=0;i<=360;i++)
         { 
-            Color drawColor = new Color(r,g,b);
+            drawColor = Art.setRainColor(i,drawColor);
             pen.turnRight(1);
             double randLength = (double)(Math.random()*360);
             Triangle testTri= new Triangle(randLength);
             testTri.draw(pen, drawColor);
-            r--;
-            g++;
-            b++;
         }
     }
 }
