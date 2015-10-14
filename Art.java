c8ecff012a6f8bc915dac12e7726d0ac7f859867
Rowan Lindsay <rowlindsay@gmail.com>
2015-10-13 20:26:32 -0700

    changed some of art

--- Art.java
+++ Art.java
@@ -6,32 +6,7 @@ public class Art
     DrawingTool pen = new DrawingTool(pad);
     public void makeArt()
     {
-       drawTractLoop(180,10);
-    }
-    
-    public void testFractal(double size,double level)
-    {
-        Triangle tri1 = new Triangle(size,Color.black);
-        tri1.draw(pen);
-        double height = (Math.tan(Math.PI/3))*(size/4);
-        pen.up();
-        pen.forward(height);
-        pen.turn(180);
-        pen.down();
-        for(int i = (int)level; i>=1; i--)
-        {
-            Color drawColor = setRainColor2(i);
-            Triangle tri2 = new Triangle(size/2,drawColor);
-            tri2.draw(pen);
-            size /= 2;
-            double moveHeight = (Math.tan(Math.PI/3))*(size/4);
-            pen.up();
-            pen.forward(moveHeight);
-            pen.turnLeft(90);
-            pen.forward(size/2);
-            pen.turnRight(90);
-            pen.down();
-        }
+       
     }
     
     public void drawTractLoop(int radius,int tractalScale)
@@ -108,24 +83,6 @@ public class Art
            b -= (int)(15/60);
         }
         Color newColor = new Color(r,g,b);
-        return newColor;
-    }
-    
-    static Color setRainColor2(int i)
-    {
-        Color newColor = Color.black; 
-        if(i==10||i==4)
-            newColor = Color.red;
-        else if(i==9||i==3)
-            newColor = Color.orange;
-        else if(i==8||i==2)
-            newColor = Color.yellow;
-        else if(i==7||i==2)
-            newColor = Color.green;
-        else if(i==6)
-            newColor = Color.blue;
-        else if(i==5)
-            newColor = Color.magenta;
         return newColor;
     }
 }
